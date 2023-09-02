133606b86c2b69fd4d8bf23c41757f69a4ad7da4
Ravi <ravi@vytec.com>
2023-09-02 12:19:23 +0530

    uodated

--- azwebapp.sh
+++ azwebapp.sh
@@ -5,7 +5,8 @@ let "randomIdentifier=$RANDOM*$RANDOM"
 location="East US"
 resourceGroup="msdocs-app-service-rg-$randomIdentifier"
 tag="deploy-github.sh"
-gitrepo=https://github.com/Azure-Samples/php-docs-hello-world # Replace the following URL with your own public GitHub repo URL if you have one
+# gitrepo=https://github.com/Azure-Samples/php-docs-hello-world # Replace the following URL with your own public GitHub repo URL if you have one
+gitrepo=https://github.com/qf-devops/azureadmin.git
 appServicePlan="msdocs-app-service-plan-$randomIdentifier"
 webapp="msdocs-web-app-$randomIdentifier"
 
@@ -23,14 +24,9 @@ az webapp create --name $webapp --resource-group $resourceGroup --plan $appServi
 
 # Deploy code from a public GitHub repository. 
 az webapp deployment source config --name $webapp --resource-group $resourceGroup \
---repo-url $gitrepo --branch master --manual-integration
+--repo-url $gitrepo --branch main --manual-integration
 
 # Use curl to see the web app.
 site="http://$webapp.azurewebsites.net"
 echo $site
 curl "$site" # Optionally, copy and paste the output of the previous command into a browser to see the web app
-
-
-
-az webapp deployment source config --name msdocs-web-app-135775266 --resource-group msdocs-app-service-rg-135775266 \
---repo-url https://github.com/qf-devops/azureadmin.git --branch main --manual-integration
