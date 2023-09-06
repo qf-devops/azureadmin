c9d666c2e5d09b16d5c7146d3c8c7d07bd095cf8
Ravi <ravi@vytec.com>
2023-09-06 12:02:25 +0530

    updated

--- acisftp.ps1
+++ acisftp.ps1
@@ -1,6 +1,6 @@
 $sftpUserName = "abcuser";
 $sftpPassword = "fj46v9hnh7f1";
 $sftpUsers = [string]::Concat($sftpUserName, ":", $sftpPassword, ":1001");
-$volumeaccountkey = "key"
+$volumeaccountkey = "aXdL2yp03by0iGQ92wRnX1OHQFyHP4Le2XhlS98/v8hVO+OLQzRCj/nAAH9A1XMImEVf9TcDh9oa+ASt+AZ8lQ=="
 
-az container create --resource-group RG-PRODUCTION-PEGA --name pega-bix-sftpserver --image "atmoz/sftp:latest" --dns-name-label "pega-bix-prod-sftp" --cpu 2 --memory 1 --os-type Linux --ports 22 --environment-variables SFTP_USERS=$sftpUsers --azure-file-volume-mount-path "/home/abcuser/dev" --azure-file-volume-share-name "dev" --azure-file-volume-account-key $volumeaccountkey --azure-file-volume-account-name "abcac" --ip-address Public --protocol TCP
+az container create --resource-group dev --name sftpserver --image "atmoz/sftp:latest" --dns-name-label "sftp-avc-1" --cpu 2 --memory 1 --os-type Linux --ports 22 --environment-variables SFTP_USERS=$sftpUsers --azure-file-volume-mount-path "/home/abcuser/dev" --azure-file-volume-share-name "devfileshare" --azure-file-volume-account-key $volumeaccountkey --azure-file-volume-account-name "sftpstoarge06092023" --ip-address Public --protocol TCP
